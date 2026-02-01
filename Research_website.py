54de5742df3d1847c5d8ec43997971ac6286f360
mazibutc <78473940+mazibutc@users.noreply.github.com>
2026-02-01 21:56:21 +0200

    Add files via upload

---
+++ Research_website.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  1 15:04:42 2026
+
+@author: Tshidiso Mazibuko
+"""
+
+import streamlit as st
+import pandas as pd
+#import plotly as px
+import plotly.express as px
+
+name = "Tshidiso Mazibuko"
+field = "Machine Learning"
+Institution = "University of South Africa (UNISA)"
+
+
+st.header("Research Overview")
+st.write(f"Name: {name}")
+st.write(f"Field of research: {field}")
+st.write(f"Institution: {Institution}")
+
+# Load data
+df_autoencoder = pd.read_excel("Book1.xlsx","Autoencoder")
+df_Dyautoencoder = pd.read_excel("Book1.xlsx","Dynamic autoencoder")
+df_LSTMautoencoder = pd.read_excel("Book1.xlsx","LSTM autoencoder")
+
+
+st.header("Results Explorer")
+# Load data
+df_autoencoder = pd.read_excel("Book1.xlsx","Autoencoder")
+df_Dyautoencoder = pd.read_excel("Book1.xlsx","Dynamic autoencoder")
+df_LSTMautoencoder = pd.read_excel("Book1.xlsx","LSTM autoencoder")
+
+st.subheader("Results viewer")
+data_option = st.selectbox("Choose the results of the machine learning algorithm", [
+    "Autoencoder", "Dynamic autoencoder","LSTM autoencoder"]
+    )
+if data_option == "Autoencoder":
+    st.write("### Autoencoder results")
+    st.dataframe(df_autoencoder)
+    df_melted = df_autoencoder.melt(id_vars=["Run"], var_name="Fault", value_name="Fault detection rate (%)")
+    #st.dataframe(df_melted )
+    fig = px.box(df_melted, x="Fault", y="Fault detection rate (%)", points="all", title="Fault-wise Box Plots")
+    st.plotly_chart(fig)
+    
+   #fig.show()
+
+elif data_option == "Dynamic autoencoder":
+    st.write("### Dynamic Autoencoder results")
+    st.dataframe(df_Dyautoencoder)
+    df_melted = df_Dyautoencoder.melt(id_vars=["Run"], var_name="Fault", value_name="Fault detection rate (%)")
+    #st.dataframe(df_melted )
+    fig = px.box(df_melted, x="Fault", y="Fault detection rate (%)", points="all", title="Fault-wise Box Plots")
+    st.plotly_chart(fig)
+    
+elif data_option == "LSTM autoencoder":
+    st.write("### LSTM Autoencoder results")
+    st.dataframe(df_LSTMautoencoder)
+    df_melted = df_LSTMautoencoder.melt(id_vars=["Run"], var_name="Fault", value_name="Fault detection rate (%)")
+    #st.dataframe(df_melted )
+    fig = px.box(df_melted, x="Fault", y="Fault detection rate (%)", points="all", title="Fault-wise Box Plots")
+    st.plotly_chart(fig)
+    
+#Add contact information
+st.header("Contact Information")
+email = "tshidisomazibuko@gmail.com"
+st.write(f"You can reach me on: {email}")
